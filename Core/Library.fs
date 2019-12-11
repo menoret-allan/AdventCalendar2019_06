f1e6572665bf20f4e4ec340878f2bec9d7ab2891
Allan Menoret <allan.menoret@gmail.com>
2019-12-11 01:22:38 +0100

    part one done

--- Core/Library.fs
+++ Core/Library.fs
@@ -1,5 +1,33 @@
 ﻿namespace Core
 
+open System
+
 module Space =
+    type OrbitSystem = {name:string; distance:int; subsystems:OrbitSystem list}
+
+    let parseRelation (relation:string) =
+        let result = relation.Split ')'
+        (result |> Seq.head, result |> Seq.last)
+
+    let parseInput (input:string) =
+        input.Split([|"\r\n";"\n"|], StringSplitOptions.RemoveEmptyEntries) |> Seq.map parseRelation |> Seq.toList
+
+    let rec addRelation a b (tree:OrbitSystem) =
+        if (tree.name = a) then {name = tree.name; distance=tree.distance;subsystems=({name=b;distance=tree.distance+1;subsystems=[]}::tree.subsystems)}
+        else {name = tree.name; distance=tree.distance; subsystems=(tree.subsystems |> Seq.map(addRelation a b) |> Seq.toList)}
+
+    let rec buildTreeRec (orbits:list<string * string>) (tree:OrbitSystem) (dones:list<string>) =
+        match orbits with
+        | [] -> tree
+        | (A, B)::rest when dones |> List.contains A -> buildTreeRec rest (addRelation A B tree) (B::dones)
+        | elem::rest -> buildTreeRec (rest @ [elem]) tree dones
+
+    let buildTree (orbits:list<string * string>) =
+        buildTreeRec orbits {name="COM"; distance=0;subsystems=[]} ["COM"]
+
+    let rec count (tree:OrbitSystem) =
+        tree.subsystems |> List.fold (fun acc subSystem -> acc + (count subSystem)) tree.distance
+
     let calculateOrbits input =
-        42
+        let tree = input |> parseInput |> buildTree
+        tree |> count
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -4,12 +4,22 @@ open Xunit
 open Core.Space
 open FsUnit
 
+
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Parse Relation 1`` () = 
+    "COM)B" |> parseRelation |> should equal ("COM", "B")
 
-
-
+[<Fact>]
+let ``Parse input`` () = 
+    let result = parseInput """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H"""
+    result |> Seq.toList |> should equivalent [("COM", "B");("B", "C");("C", "D");("D", "E");("E", "F");("B", "G");("G", "H")]
 
 
 
@@ -30,3 +40,20 @@ E)J
 J)K
 K)L"""
     input |> calculateOrbits |> should equal 42
+
+
+[<Fact>]
+let ``Calculate total orbits direct and indirect with unordered input`` () =
+    let input = """D)I
+B)C
+C)D
+E)F
+B)G
+G)H
+COM)B
+E)J
+J)K
+D)E
+K)L"""
+    input |> calculateOrbits |> should equal 42
+
